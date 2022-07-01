59c3b196a7d7f985dcb31af20a44771b3c73be2a
narayani2327 <harikalanarayani@gmail.com>
2022-07-01 15:23:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <Navbar/>
     </div>
   );
 }
-
 export default App;
